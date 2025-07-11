bf4478a0d6e40be05c0b47b96fddbf487c9a1384
Michal Mrkos <michalmrkos@Michals-MacBook-Air.local>
2025-07-11 11:23:55 +0200

    steps proj

---
+++ MY_CODE/steps/src/App.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+  return (
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step === 1 ? "active" : ""}`}>1</div>
+            <div className={`${step === 2 ? "active" : ""}`}>2</div>
+            <div className={`${step === 3 ? "active" : ""}`}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button onClick={() => setStep(Math.max(step - 1, 1))}>prev</button>
+            <button onClick={() => setStep(Math.min(step + 1, 3))}>next</button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
